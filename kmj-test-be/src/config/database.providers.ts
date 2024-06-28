ec48bbcc2ffc28385ce65dac576bfc92bce9e929
Avetis Muradyan <avetismu@gmail.com>
2024-06-28 22:02:39 +0800

    [🔨Feature / Fonctionnalité] Added Database Configuration / Configuration de la Base de Donnée

---
+++ kmj-test-be/src/config/database.providers.ts
@@ -0,0 +1,26 @@
+import { DataSource } from 'typeorm';
+import { config } from 'dotenv';
+
+config();
+
+export const databaseProviders = [
+  {
+    provide: 'DATA_SOURCE',
+    useFactory: async () => {
+      const dataSource = new DataSource({
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT, 10),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        entities: [
+            __dirname + '/../**/*.entity{.ts,.js}',
+        ],
+        synchronize: true,
+      });
+
+      return dataSource.initialize();
+    },
+  },
+];
