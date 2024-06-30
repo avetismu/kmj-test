893ad58069e7c0f37f6ec354549141cc2fb19b0f
Avetis Muradyan <avetismu@gmail.com>
2024-06-30 17:10:22 +0800

    [🔨Feature / Fonctionnalité] event Entity implemented / Implementation de l'Entity event

--- kmj-test-be/src/resource/event/entities/event.entity.ts
+++ kmj-test-be/src/resource/event/entities/event.entity.ts
@@ -1,1 +1,25 @@
-export class Event {}
+import { UUID } from "crypto";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Timezone } from "../utils/enum.utils";
+
+@Entity('event')
+export class Event {
+
+    @PrimaryGeneratedColumn('uuid')
+    uuid: UUID;
+
+    @Column('varchar', {name: 'title', length: 255, nullable : false})
+    title: string;
+
+    @Column('text', {name: 'description', nullable : false})
+    description: string;
+
+    @Column('timestamptz', {'name': 'start', nullable : false})
+    start: Date;
+
+    @Column('timestamptz', {'name': 'end', nullable : false})
+    end: Date;
+
+    @Column('enum', {'name': 'timezone', 'enum': Timezone, 'default': Timezone.GMT, nullable : false})
+    timezone: Timezone;
+}
