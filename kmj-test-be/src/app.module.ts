1f779b6cf5e123b587be7261b543e47189b9dca4
Avetis Muradyan <avetismu@gmail.com>
2024-06-30 16:47:20 +0800

    [⚙️ Refactor / Réusinage] Removal of Hello World / Élimination de Hello World

--- kmj-test-be/src/app.module.ts
+++ kmj-test-be/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { EventModule } from './resource/event/event.module';
 
 @Module({
   imports: [EventModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
