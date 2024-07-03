e1316d4ba397fc1a3f8874e6a41d0b3ac8bbacdb
Avetis Muradyan <avetismu@gmail.com>
2024-07-03 10:43:00 -0700

    🔨Feature / Fonctionnalité] Added basic tests for event service / Ajout de tests de base pour le service event

--- kmj-test-be/src/resource/event/event.service.spec.ts
+++ kmj-test-be/src/resource/event/event.service.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EventService } from './event.service';
+import { time } from 'console';
+import { Timezone } from './utils/enum.utils';
 
 describe('EventService', () => {
   let service: EventService;
@@ -12,7 +14,31 @@ describe('EventService', () => {
     service = module.get<EventService>(EventService);
   });
 
+
+  // Mock Test Data
+  const eventData = {
+    title: 'Test Event',
+    description: 'Test Description',
+    start: new Date(),
+    end: new Date(),
+    timezone: Timezone.GMT,
+  }
+
+
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  // Trivial findAll Test
+  it('should return an array of events', () => {
+    expect(service.findAll()).toBeInstanceOf(Array);
+    }
+  );
+
+  // Trivial create Test
+  it('should create an event', () => {
+    expect(service.create(eventData)).toBeInstanceOf(Object);
+    }
+  );
+
 });
