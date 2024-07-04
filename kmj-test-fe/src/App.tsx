187c4cc13d26628d771591350ec78e6dd341854b
Avetis Muradyan <avetismu@gmail.com>
2024-07-03 20:22:50 -0700

    🔨Feature / Fonctionnalité] Router added / Ajout du Router

--- kmj-test-fe/src/App.tsx
+++ kmj-test-fe/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './routes/router';
 
 function App() {
   return (
     <div className="App">
+      <RouterProvider router={router}/>
       <header className="App-header">
       </header>
     </div>
---
+++ kmj-test-fe/src/routes/router.tsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const Router = createBrowserRouter([
+    {
+      path: "/",
+      element: <h2>Hellow World</h2>,
+    },
+  ]);
+
+  export default Router;
