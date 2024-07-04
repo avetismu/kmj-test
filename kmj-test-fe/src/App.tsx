6e03ed7198f2d2e61bcd5a8568e63035971bceeb
Avetis Muradyan <avetismu@gmail.com>
2024-07-03 20:25:45 -0700

    [🔨Feature / Fonctionnalité] Redux Store added / Ajout du Store Redux]

--- kmj-test-fe/src/App.tsx
+++ kmj-test-fe/src/App.tsx
@@ -3,13 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes/router';
+import { Provider } from 'react-redux';
+import store from './store/store';
 
 function App() {
   return (
     <div className="App">
-      <RouterProvider router={router}/>
-      <header className="App-header">
-      </header>
+      <Provider store={store}>
+        <RouterProvider router={router}/>
+        <header className="App-header">
+        </header>
+      </Provider>
     </div>
   );
 }
