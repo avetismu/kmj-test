627acef59a6ffa0baace32010c330a69a93350c6
Avetis Muradyan <avetismu@gmail.com>
2024-07-04 04:27:05 -0700

    [⚙️ Refactor / Réusinage] Removed unused dependency / Suppression de dépendance superflu

--- kmj-test-fe/src/App.tsx
+++ kmj-test-fe/src/App.tsx
@@ -1,7 +1,5 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, RouterProvider } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 import router from './routes/router';
 import { Provider } from 'react-redux';
 import store from './store/store';
